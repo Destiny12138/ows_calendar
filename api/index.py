2f00c763904c27df7005bc9e5c00cac4a95b6cd3
Destiny12138 <81007141+Destiny12138@users.noreply.github.com>
2023-01-13 20:44:48 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask import send_file
 import requests
 import io
+import datetime
+
 app = Flask(__name__)
 
 # @app.route('/')
@@ -16,8 +18,11 @@ def index():
     header = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.25 Safari/537.36 Core/1.70.3868.400 QQBrowser/10.8.4394.400"
     }  # 发送头信息
- 
-    req = requests.get(url="http://img.owspace.com/Public/uploads/Download/2023/0113.jpg", headers=header)
+    today = str(datetime.date.today())
+    year = today[0:4]
+    mon = today[5:7]+today[8:]
+    req = requests.get(
+        url=f"http://img.owspace.com/Public/uploads/Download/{year}/{mon}.jpg", headers=header)
     byte = req.content
     print(byte)
     # image = file("http://img.owspace.com/Public/uploads/Download/2023/0113.jpg")
